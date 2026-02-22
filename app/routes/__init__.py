db086d6db0a14bde662eea4b81cfbc8f504ec3e1
Railagg <agraii161@gmail.com>
2026-02-22 13:49:42 +0700

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,5 @@
-
+from .game import router as game_router
+from .pvp import router as pvp_router
+from .guild import router as guild_router
+from .premium import router as premium_router
+from .nft import router as nft_router
