b17d1b5f9e4c06cb179021749422cf3254a4811d
Railagg <agraii161@gmail.com>
2026-02-22 18:34:11 +0700

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,1 +1,7 @@
+from . import start, game, pets, exchange, rainbow, premium, nft, guild, pvp, codex, events, shop, top, admin
 
+__all__ = [
+    "start", "game", "pets", "exchange", "rainbow", 
+    "premium", "nft", "guild", "pvp", "codex", 
+    "events", "shop", "top", "admin"
+]
