6a6799c5bf0cef0e36e850895602da46f3b2f81d
Railagg <agraii161@gmail.com>
2026-02-22 14:42:08 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routes import game, pvp, guild, premium, nft
+import utils
 
 # Создаем таблицы в базе данных
 Base.metadata.create_all(bind=engine)
